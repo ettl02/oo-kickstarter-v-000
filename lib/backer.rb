a88f98fd29199b15163399ce5e6e9d82c4836348
Emily Wessel <ettl02@gmail.com>
2018-07-05 17:41:25 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,8 @@
 class Backer
-  attr_accessor :name, :backed_projects
+  attr_accessor :name, :back_projects
 
   def initialize (name)
     @name = name
-    @backed_projects = []
+    @back_projects = []
   end
 end
